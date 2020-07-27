53805cc56b17985fed075ceedfbf7ddf9982850b
NC05UV <xavier.michel.mayeur@ing.com>
2020-07-27 15:27:03 +0200

    Added the header merge functionality

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-"C:\Users\XY56RE\Programs\python37\scripts\cmd.bat" "cd %~dp0 && pyinstaller -F y2j.spec && copy /y dist\y2j.exe C:\Users\XY56RE\Programs""
+cd %~dp0
+rem venv\scripts\activate
+pip install -r requirements.txt
+pyinstaller -F y2j.spec && copy /y dist\y2j.exe %appdata%\..\local\Programs
