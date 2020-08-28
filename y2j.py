d81bdc4928f375993d2126842f0c52a99b865743
NC05UV <xavier.michel.mayeur@ing.com>
2020-08-28 14:45:30 +0200

    Fix bug in directory conversion

--- y2j.py
+++ y2j.py
@@ -368,7 +368,8 @@ def main():
                 ext = f.split('.')[1]
             else:
                 ext = ''
-            if args.json or ext == 'json' or ext == 'avsc':
+            if args.json:
+                if ext == 'json' or ext == 'avsc':
                     o = join(args.directory, f.split('.')[0] + '.yaml')
                     print(f'Converting file {f}')
                     convert(join(args.directory, f), o, True)
